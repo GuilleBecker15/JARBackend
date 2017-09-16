0c57ff173ce4c0aede7e8b3757df57cc05f94d5b
guillebecker15@gmail.com <guille.becker.15@gmail.com>
2017-09-15 22:45:39 -0300

    metodo comprobar assa

--- app/controllers/usuarios/confirmations_controller.rb
+++ app/controllers/usuarios/confirmations_controller.rb
@@ -4,9 +4,13 @@ class Usuarios::ConfirmationsController < Devise::ConfirmationsController
     # self.resource = resource_class.send_confirmation_instructions(resource_params)
     self.resource = resource_class.send_confirmation_instructions({email: current_user.email})
     if successfully_sent?(resource)
+      p "--------------------------------------CONFIRMATION CREATE IF--------------------------------------------------------------"
       respond_with({}, :location => after_resending_confirmation_instructions_path_for(resource_name))
+      p "----------------------------------------------------------------------------------------------------"
     else
+      p "--------------------------------------CONFIRMATION CREATE ELSE--------------------------------------------------------------"
       respond_with(resource)
+      p "----------------------------------------------------------------------------------------------------"
     end
   end
 
